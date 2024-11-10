7a06b9241b3fd2a715bf62d195895becdf27d94d
José A. Montes Pérez <jmontp@umich.edu>
2024-11-10 14:40:56 -0500

    Actual updates to file

--- AddBiomechanics/extract_biomechanics_dataset.sh
+++ AddBiomechanics/extract_biomechanics_dataset.sh
@@ -7,3 +7,13 @@ for f in *.zip; do
     unzip "$f" -d "$common_folder" 
     find "$common_folder" -name "*.b3d" -exec cp {} "$common_folder" \; 
 done
+
+# Delete any remaining folders inside the common folder
+rm -r "$common_folder"/*/
+
+# Print finished
+echo "Finished extracting"
+
+# Move the common folder to /datasets/AddBiomechanics/raw_data
+mv "$common_folder" /datasets/AddBiomechanics/raw_data
+echo "Moved to /datasets/AddBiomechanics/raw_data"
